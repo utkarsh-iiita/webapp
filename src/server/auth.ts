34ef263248a87a2a8d0ca2bce1b19cf27bbbce78
BuddyLongLegs <anurag10jain@gmail.com>
2023-12-24 19:09:50 +0530

    added support of .env.local (mandatory)

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -1,7 +1,6 @@
 import CredentialsProvider from 'next-auth/providers/credentials';
 import { DrizzleAdapter } from "@auth/drizzle-adapter";
 import {
-  getServerSession,
   type DefaultSession,
   type NextAuthOptions,
 } from "next-auth";
@@ -10,12 +9,6 @@ import { env } from "~/env";
 import { db } from "~/server/db";
 import { mysqlTable } from "~/server/db/schema";
 
-/**
- * Module augmentation for `next-auth` types. Allows us to add custom properties to the `session`
- * object and keep type safety.
- *
- * @see https://next-auth.js.org/getting-started/typescript#module-augmentation
- */
 declare module "next-auth" {
   interface Session extends DefaultSession {
     user: {
@@ -24,18 +17,8 @@ declare module "next-auth" {
       // role: UserRole;
     } & DefaultSession["user"];
   }
-
-  // interface User {
-  //   // ...other properties
-  //   // role: UserRole;
-  // }
 }
 
-/**
- * Options for NextAuth.js used to configure adapters, providers, callbacks, etc.
- *
- * @see https://next-auth.js.org/configuration/options
- */
 export const authOptions: NextAuthOptions = {
   callbacks: {
     session: ({ session, user }) => ({
@@ -60,20 +43,6 @@ export const authOptions: NextAuthOptions = {
         return { id: "1", name: "Utkarsh" };
       },
     }),
-    /**
-     * ...add more providers here.
-     *
-     * Most other providers require a bit more work than the Discord provider. For example, the
-     * GitHub provider requires you to add the `refresh_token_expires_in` field to the Account
-     * model. Refer to the NextAuth.js docs for the provider you want to use. Example:
-     *
-     * @see https://next-auth.js.org/providers/github
-     */
+
   ],
 };
-
-/**
- * Wrapper for `getServerSession` so that you don't need to import the `authOptions` in every file.
- *
- * @see https://next-auth.js.org/configuration/nextjs
- */
--- src/server/db/schema/index.ts
+++ src/server/db/schema/index.ts
@@ -1,5 +1,6 @@
 import { mysqlTableCreator } from 'drizzle-orm/mysql-core';
+import { type MySqlTableFn } from 'drizzle-orm/mysql-core';
 
 export * from '~/server/db/schema/user'
 
-export const mysqlTable = mysqlTableCreator((name) => `utkarsh-portal_${name}`);
+export const mysqlTable: MySqlTableFn = mysqlTableCreator((name) => `utkarsh-portal_${name}`);
