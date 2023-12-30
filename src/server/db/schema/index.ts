4425df08e623f7095203fb167e141cd4c64db453
BuddyLongLegs <anurag10jain@gmail.com>
2023-12-30 21:02:22 +0530

    removed old files

--- src/server/db/schema/index.ts
+++
@@ -1,6 +0,0 @@
-import { mysqlTableCreator } from 'drizzle-orm/mysql-core';
-import { type MySqlTableFn } from 'drizzle-orm/mysql-core';
-
-export * from '~/server/db/schema/user'
-
-export const mysqlTable: MySqlTableFn = mysqlTableCreator((name) => `utkarsh-portal_${name}`);
