57f4c1910092f777b8bdeade43571bda6df05981
BuddyLongLegs <anurag10jain@gmail.com>
2024-04-04 14:57:41 +0530

    post page fixes

--- src/app/(main)/(protected)/admin/post/page.tsx
+++ src/app/(main)/(protected)/admin/post/page.tsx
@@ -1,48 +1,29 @@
-"use client"
+"use client";
 
-import { Box, CircularProgress, Container, Divider, TextField, Typography } from "@mui/material";
+import {
+  Box,
+  CircularProgress,
+  Container,
+  Divider,
+  TextField,
+  Typography,
+} from "@mui/material";
 
-import { api } from "~/trpc/react"
+import { api } from "~/trpc/react";
+
 import PostRow from "./_components/PostRow";
 
-
 function Page() {
-  const { data: allPosts, isLoading } = api.post.getLatestPost.useQuery();
+  const { data: allPosts, isLoading } = api.post.getLatestPost.useQuery({});
 
   return (
     <>
-      <Typography variant="subtitle1" className="-mt-2" color="primary">
-        All Posts
-      </Typography>
-      <Divider className="mb-4 mt-2" />
-      <Container maxWidth="xl" className="m-0 p-0">
-        <Box className="mb-4 flex md:flex-row flex-col gap-4 md:gap-2 md:items-center">
-          <div className="flex flex-nowrap flex-grow w-full">
-            {/* <TextField
-              label="Search"
-              variant="outlined"
-              size="small"
-              className="flex-grow mr-1"
-              onChange={(e) => setSearch(e.target.value)}
-              onKeyDown={(e) => {
-                if (e.key === 'Enter') {
-                  handleSearch();
-                }
-              }}
-            /> */}
-            {/* <IconButton
-              sx={{
-                backgroundColor: 'primary.main',
-                borderRadius: '8px',
-                marginRight: '4px',
-              }}
-              onClick={handleSearch}
-            >
-              <SearchIcon />
-            </IconButton> */}
-          </div>
-        </Box>
-        {(isLoading) && (
+      <Container className="flex flex-col gap-4 py-4">
+        <Typography variant="h5" color="primary" className="px-4">
+          All Posts
+        </Typography>
+        <Divider />
+        {isLoading && (
           <Container className="h-96 w-full flex justify-center items-center">
             <CircularProgress />
           </Container>
@@ -52,30 +33,17 @@ function Page() {
             {allPosts &&
               allPosts.map((post) => (
                 <PostRow
-
                   id={post.id}
+                  key={post.id}
                   title={post.title}
                   createdAt={post.createdAt}
                 />
               ))}
-
-
           </Box>
         }
-
       </Container>
     </>
-
-
-
-
-
-
-
-
-
-
-  )
+  );
 }
 
-export default Page
+export default Page;
