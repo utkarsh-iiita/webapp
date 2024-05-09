d288525105ada96a1e9256889dd25c55ff4b835f
BuddyLongLegs <anurag10jain@gmail.com>
2024-05-09 14:16:59 +0530

    added loading state in new job opening

--- src/app/(main)/(protected)/admin/job-openings/new/page.tsx
+++ src/app/(main)/(protected)/admin/job-openings/new/page.tsx
@@ -374,6 +374,7 @@ export default function NewJobOpening() {
             type="submit"
             variant="contained"
             disabled={isCreationDisabled}
+            loading={createJobOpeningMutation.isLoading}
           >
             Create
           </LoadingButton>
